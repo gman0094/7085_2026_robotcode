a3346a6e297c28dc1f79e8becde86d5c934fb20a
gman0094 <g.coats09@gmail.com>
2026-03-09 19:49:32 -0400

    vis

--- src/main/java/frc/robot/subsystems/vision/VisionIOPhotonVisionSim.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIOPhotonVisionSim.java
@@ -1,54 +1,124 @@
-// Copyright (c) 2021-2026 Littleton Robotics
-// http://github.com/Mechanical-Advantage
-//
-// Use of this source code is governed by a BSD
-// license that can be found in the LICENSE file
-// at the root directory of this project.
-
 package frc.robot.subsystems.vision;
 
 import static frc.robot.subsystems.vision.VisionConstants.aprilTagLayout;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform3d;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.function.Supplier;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.simulation.PhotonCameraSim;
 import org.photonvision.simulation.SimCameraProperties;
 import org.photonvision.simulation.VisionSystemSim;
+import org.photonvision.targeting.PhotonPipelineResult;
 
-/** IO implementation for physics sim using PhotonVision simulator. */
-public class VisionIOPhotonVisionSim extends VisionIOPhotonVision {
+public class VisionIOPhotonVisionSim implements VisionIO {
   private static VisionSystemSim visionSim;
 
-  private final Supplier<Pose2d> poseSupplier;
+  private final PhotonCamera camera;
   private final PhotonCameraSim cameraSim;
+  private final Transform3d robotToCamera;
+  private final PhotonPoseEstimator poseEstimator;
+  private final Supplier<Pose2d> robotPoseSupplier;
 
-  /**
-   * Creates a new VisionIOPhotonVisionSim.
-   *
-   * @param name The name of the camera.
-   * @param poseSupplier Supplier for the robot pose to use in simulation.
-   */
   public VisionIOPhotonVisionSim(
-      String name, Transform3d robotToCamera, Supplier<Pose2d> poseSupplier) {
-    super(name, robotToCamera);
-    this.poseSupplier = poseSupplier;
+      String cameraName, Transform3d robotToCamera, Supplier<Pose2d> robotPoseSupplier) {
+    this.camera = new PhotonCamera(cameraName);
+    this.robotToCamera = robotToCamera;
+    this.robotPoseSupplier = robotPoseSupplier;
 
-    // Initialize vision sim
     if (visionSim == null) {
       visionSim = new VisionSystemSim("main");
       visionSim.addAprilTags(aprilTagLayout);
     }
 
-    // Add sim camera
-    var cameraProperties = new SimCameraProperties();
-    cameraSim = new PhotonCameraSim(camera, cameraProperties, aprilTagLayout);
+    SimCameraProperties cameraProps = new SimCameraProperties();
+    cameraProps.setCalibration(960, 720, Rotation2d.fromDegrees(90.0));
+    cameraProps.setCalibError(0.35, 0.10);
+    cameraProps.setFPS(30);
+    cameraProps.setAvgLatencyMs(35);
+    cameraProps.setLatencyStdDevMs(5);
+
+    cameraSim = new PhotonCameraSim(camera, cameraProps);
     visionSim.addCamera(cameraSim, robotToCamera);
+
+    poseEstimator =
+        new PhotonPoseEstimator(
+            aprilTagLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, robotToCamera);
+    poseEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
   }
 
   @Override
   public void updateInputs(VisionIOInputs inputs) {
-    visionSim.update(poseSupplier.get());
-    super.updateInputs(inputs);
+    visionSim.update(robotPoseSupplier.get());
+
+    inputs.connected = true;
+    inputs.latestTargetObservation = new TargetObservation(-1, new Rotation2d());
+    inputs.targetObservations = new TargetObservation[] {};
+    inputs.tagIds = new int[] {};
+    inputs.poseObservations = new PoseObservation[] {};
+
+    PhotonPipelineResult result = camera.getLatestResult();
+
+    List<TargetObservation> targetObservations = new LinkedList<>();
+    List<Integer> tagIds = new LinkedList<>();
+    List<PoseObservation> poseObservations = new LinkedList<>();
+
+    if (result.hasTargets()) {
+      var bestTarget = result.getBestTarget();
+      inputs.latestTargetObservation =
+          new TargetObservation(
+              bestTarget.getFiducialId(), Rotation2d.fromDegrees(bestTarget.getYaw()));
+
+      for (var target : result.getTargets()) {
+        int id = target.getFiducialId();
+        Rotation2d tx = Rotation2d.fromDegrees(target.getYaw());
+
+        targetObservations.add(new TargetObservation(id, tx));
+
+        if (id >= 0) {
+          tagIds.add(id);
+        }
+      }
+    }
+
+    var estimatedPose = poseEstimator.update(result);
+    if (estimatedPose.isPresent()) {
+      var estimate = estimatedPose.get();
+
+      double totalDistance = 0.0;
+      int validTagCount = 0;
+      double ambiguity = 0.0;
+
+      for (var target : result.getTargets()) {
+        if (target.getFiducialId() >= 0) {
+          totalDistance += target.getBestCameraToTarget().getTranslation().getNorm();
+          validTagCount++;
+        }
+      }
+
+      if (result.hasTargets() && result.getTargets().size() == 1) {
+        ambiguity = result.getBestTarget().getPoseAmbiguity();
+      }
+
+      double averageTagDistance = validTagCount > 0 ? totalDistance / validTagCount : 0.0;
+
+      poseObservations.add(
+          new PoseObservation(
+              estimate.timestampSeconds,
+              estimate.estimatedPose,
+              ambiguity,
+              validTagCount,
+              averageTagDistance,
+              PoseObservationType.PHOTONVISION));
+    }
+
+    inputs.targetObservations = targetObservations.toArray(new TargetObservation[0]);
+    inputs.tagIds = tagIds.stream().mapToInt(Integer::intValue).toArray();
+    inputs.poseObservations = poseObservations.toArray(new PoseObservation[0]);
   }
 }
